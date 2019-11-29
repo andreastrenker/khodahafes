bb260ea2413fa0fb408715bd2e14ce78b801296a
Nuel van Gelder de Neufville <github@nuel.work>
2019-11-29 17:22:05 +0100

    Add audio component, close #2

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 /* global Vue */
 const images = [
-  [{ size: 2 }, { size: 1 }, { size: 3 }],
-  [{ size: 1 }, { size: 2 }, { size: 1 }],
+  [{ size: 2, noaudio: true }, { size: 1, noaudio: true }, { size: 3 }],
+  [{ size: 1 }, { size: 2 }, { size: 1, noaudio: true }],
   [{ size: 3 }, { size: 3 }],
   [{ size: 1 }, { size: 2 }, { size: 1 }],
-  [{ size: 2 }, { size: 3 }, { size: 1 }],
+  [{ size: 2 }, { size: 3, noaudio: true }, { size: 1 }],
   [{ size: 3 }, { size: 1 }],
   [{ size: 2 }, { size: 3 }],
   [{ size: 1 }],
-  [{ size: 2 }, { size: 3 }]
+  [{ size: 2, noaudio: true }, { size: 3 }]
 ]
 
 let index = 0
@@ -17,10 +17,28 @@ for (column of images) {
   for (image of column) {
     index ++
     image.index = index
-    console.log(index)
   }
 }
 
+Vue.component('audio-player', {
+  props: ['image'],
+  data: () => {
+    return {
+      playing: false
+    }
+  },
+  template: `
+    <div @click="toggle">
+      <img :src="'images/audio-' + (playing ? 'on' : 'off') + '.png'">
+    </div>
+  `,
+  methods: {
+    toggle: function () {
+      this.playing = !this.playing
+      document.getElementById('audio-' + this.$props.image)[this.playing ? 'play' : 'pause']()
+    }
+  }
+})
 
 const app = new Vue({
   el: '#app',
