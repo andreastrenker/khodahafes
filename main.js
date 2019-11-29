b596498731db0c76afce8da98bb0f631d388abf0
Nuel van Gelder de Neufville <github@nuel.work>
2019-11-29 16:20:45 +0100

    Create mapping for images, closes #3

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 /* global Vue */
 const images = [
-  {}
+  [{ size: 2 }, { size: 1 }, { size: 3 }],
+  [{ size: 1 }, { size: 2 }, { size: 1 }],
+  [{ size: 3 }, { size: 3 }],
+  [{ size: 1 }, { size: 2 }, { size: 1 }],
+  [{ size: 2 }, { size: 3 }, { size: 1 }],
+  [{ size: 3 }, { size: 1 }],
+  [{ size: 2 }, { size: 3 }],
+  [{ size: 1 }],
+  [{ size: 2 }, { size: 3 }]
 ]
+
+let index = 0
+
+for (column of images) {
+  for (image of column) {
+    index ++
+    image.index = index
+    console.log(index)
+  }
+}
+
 
 const app = new Vue({
   el: '#app',
-  data: {}
+  data: {
+    images
+  }
 })
