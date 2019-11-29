ec85b46c19dfdb5e167f5f9299d0d3f4a7b32a14
Nuel van Gelder de Neufville <github@nuel.work>
2019-11-29 17:59:29 +0100

    Add progress bar, close #5

--- main.js
+++ main.js
@@ -24,19 +24,56 @@ Vue.component('audio-player', {
   props: ['image'],
   data: () => {
     return {
-      playing: false
+      playing: false,
+      timer: null,
+      audio: null,
+      progress: 0,
+      percent: 0
     }
   },
   template: `
     <div @click="toggle">
+      <div class="progress-bar" :style="'width: ' + percent + '%'"></div>
       <img :src="'images/audio-' + (playing ? 'on' : 'off') + '.png'">
     </div>
   `,
   methods: {
     toggle: function () {
       this.playing = !this.playing
-      document.getElementById('audio-' + this.$props.image)[this.playing ? 'play' : 'pause']()
+
+      if (!this.audio) {
+        return
+      }
+
+      this.audio[this.playing ? 'play' : 'pause']()
+    },
+
+    advance: function (duration, element) {
+      const increment = 10 / duration
+      this.percent = Math.min(increment * element.currentTime * 10, 100)
+      this.startTimer(duration, element)
+    },
+    startTimer: function (duration, element) {
+      if (this.percent < 99) {
+        this.timer = setTimeout(() => {
+          this.advance(duration, element)
+        }, 100)
+      } else {
+        this.progress = 0
+        this.percent = 0
+        this.playing = false
+      }
     }
+  },
+  mounted: function () {
+    this.audio = document.getElementById('audio-' + this.$props.image)
+    this.audio.addEventListener('playing', (_event) => {
+      const duration = _event.target.duration
+      this.advance(duration, this.audio)
+    })
+    this.audio.addEventListener('pause', (_event) => {
+      clearTimeout(this.timer)
+    })
   }
 })
 
